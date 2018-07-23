bb18e1c75f9466221f5036ed95f9138138e52951
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-07-23 16:47:31 -0400

    spp definitions

---
+++ sppModel_independent.R
@@ -0,0 +1,153 @@
+
+#define species functions
+# 100 high response, linear functions Factor 1
+# 50 positive, 50 negative
+
+
+spp1<-function(a,b,c,d,e) {10000+a*1000+(0*(a+b+c+d+e))}
+spp2<-function(a,b,c,d,e) {10000-a*1000+(0*(a+b+c+d+e))}
+spp3<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp4<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp5<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp6<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp7<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp8<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp9<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+spp10<-function(a,b,c,d,e) {+(0*(a+b+c+d+e))}
+
+#random/no-response
+
+spp11<-function(a,b,c,d,e) {rnorm(1, 50, 50)+(0*(a+b+c+d+e))} #generate 1 number with mean=50, stdev=50
+spp12<-function(a,b,c,d,e) {rnorm(1, 40, 40)+(0*(a+b+c+d+e))}
+spp13<-function(a,b,c,d,e) {rnorm(1, 20, 20)+(0*(a+b+c+d+e))}
+spp14<-function(a,b,c,d,e) {rnorm(1, 10, 10)+(0*(a+b+c+d+e))}
+spp15<-function(a,b,c,d,e) {rnorm(1, 50, 100)+(0*(a+b+c+d+e))}
+spp16<-function(a,b,c,d,e) {rnorm(1, 5, 200)+(0*(a+b+c+d+e))}
+spp17<-function(a,b,c,d,e) {rnorm(1, 5, 100)+(0*(a+b+c+d+e))}
+spp18<-function(a,b,c,d,e) {rnorm(1, 20, 50)+(0*(a+b+c+d+e))}
+spp19<-function(a,b,c,d,e) {rnorm(1, 5, 50)+(0*(a+b+c+d+e))}
+spp20<-function(a,b,c,d,e) {rnorm(1, 5, 5)+(0*(a+b+c+d+e))}
+
+spp21<-function(a,b,c,d,e) {rnorm(1, 5, 50)+(0*(a+b+c+d+e))}
+spp22<-function(a,b,c,d,e) {rnorm(1, 5, 50)+(0*(a+b+c+d+e))}
+spp23<-function(a,b,c,d,e) {rnorm(1, 5, 50)+(0*(a+b+c+d+e))}
+spp24<-function(a,b,c,d,e) {rnorm(1, 2, 50)+(0*(a+b+c+d+e))}
+spp25<-function(a,b,c,d,e) {rnorm(1, 2, 50)+(0*(a+b+c+d+e))}
+spp26<-function(a,b,c,d,e) {rnorm(1, 1, 50)+(0*(a+b+c+d+e))}
+spp27<-function(a,b,c,d,e) {rnorm(1, 1, 50)+(0*(a+b+c+d+e))}
+spp28<-function(a,b,c,d,e) {rnorm(1, 10, 50)+(0*(a+b+c+d+e))}
+spp29<-function(a,b,c,d,e) {rnorm(1, 10, 50)+(0*(a+b+c+d+e))}
+spp30<-function(a,b,c,d,e) {rnorm(1, 1, 100)+(0*(a+b+c+d+e))}
+spp31<-function(a,b,c,d,e) {rnorm(1, 1, 100)+(0*(a+b+c+d+e))}
+spp32<-function(a,b,c,d,e) {rnorm(1, 1, 5)+(0*(a+b+c+d+e))}
+
+
+
+#linear functions
+spp33<-function(a,b,c,d,e) {10*a+0*b*c*d*e}
+spp34<-function(a,b,c,d,e) {10*b+0*a*c*d*e}
+spp35<-function(a,b,c,d,e) {10*c+0*b*a*d*e}
+spp36<-function(a,b,c,d,e) {}
+spp37<-function(a,b,c,d,e) {}
+spp38<-function(a,b,c,d,e) {}
+spp6<-function(a,b,c,d,e) {}
+spp7<-function(a,b,c,d,e) {}
+spp8<-function(a,b,c,d,e) {}
+spp9<-function(a,b,c,d,e) {}
+spp10<-function(a,b,c,d,e) {}
+spp11<-function(a,b,c,d,e) {}
+spp12<-function(a,b,c,d,e) {}
+spp13<-function(a,b,c,d,e) {}
+spp14<-function(a,b,c,d,e) {}
+
+#exponential functions
+spp15<-function(a,b,c,d,e) {}
+spp16<-function(a,b,c,d,e) {}
+spp17<-function(a,b,c,d,e) {}
+spp18<-function(a,b,c,d,e) {}
+spp19<-function(a,b,c,d,e) {}
+spp20<-function(a,b,c,d,e) {}
+spp21<-function(a,b,c,d,e) {}
+spp22<-function(a,b,c,d,e) {}
+spp23<-function(a,b,c,d,e) {}
+spp24<-function(a,b,c,d,e) {}
+spp25<-function(a,b,c,d,e) {}
+spp26<-function(a,b,c,d,e) {}
+spp27<-function(a,b,c,d,e) {}
+spp28<-function(a,b,c,d,e) {}
+spp29<-function(a,b,c,d,e) {}
+spp30<-function(a,b,c,d,e) {}
+spp31<-function(a,b,c,d,e) {}
+spp32<-function(a,b,c,d,e) {}
+
+Parabolic functions
+spp33<-function(a,b,c,d,e) {}
+spp34<-function(a,b,c,d,e) {}
+spp35<-function(a,b,c,d,e) {}
+spp36<-function(a,b,c,d,e) {}
+spp37<-function(a,b,c,d,e) {}
+spp38<-function(a,b,c,d,e) {}
+spp39<-function(a,b,c,d,e) {}
+spp40<-function(a,b,c,d,e) {}
+spp41<-function(a,b,c,d,e) {}
+spp42<-function(a,b,c,d,e) {}
+spp43<-function(a,b,c,d,e) {}
+spp44<-function(a,b,c,d,e) {}
+spp45<-function(a,b,c,d,e) {}
+spp46<-function(a,b,c,d,e) {}
+spp47<-function(a,b,c,d,e) {}
+spp48<-function(a,b,c,d,e) {}
+spp49<-function(a,b,c,d,e) {}
+spp50<-function(a,b,c,d,e) {}
+
+#Multinomial (e.g. response to substrate input)
+spp51<-function(a,b,c,d,e) {}
+spp52<-function(a,b,c,d,e) {}
+spp53<-function(a,b,c,d,e) {}
+spp54<-function(a,b,c,d,e) {}
+spp55<-function(a,b,c,d,e) {}
+spp56<-function(a,b,c,d,e) {}
+spp57<-function(a,b,c,d,e) {}
+spp58<-function(a,b,c,d,e) {}
+spp59<-function(a,b,c,d,e) {}
+spp60<-function(a,b,c,d,e) {}
+spp61<-function(a,b,c,d,e) {}
+spp62<-function(a,b,c,d,e) {}
+spp63<-function(a,b,c,d,e) {}
+spp64<-function(a,b,c,d,e) {}
+spp65<-function(a,b,c,d,e) {}
+spp66<-function(a,b,c,d,e) {}
+spp67<-function(a,b,c,d,e) {}
+spp68<-function(a,b,c,d,e) {}
+spp69<-function(a,b,c,d,e) {}
+spp70<-function(a,b,c,d,e) {}
+spp71<-function(a,b,c,d,e) {}
+spp72<-function(a,b,c,d,e) {}
+spp73<-function(a,b,c,d,e) {}
+spp74<-function(a,b,c,d,e) {}
+spp75<-function(a,b,c,d,e) {}
+spp76<-function(a,b,c,d,e) {}
+spp77<-function(a,b,c,d,e) {}
+spp78<-function(a,b,c,d,e) {}
+spp79<-function(a,b,c,d,e) {}
+spp80<-function(a,b,c,d,e) {}
+spp81<-function(a,b,c,d,e) {}
+spp82<-function(a,b,c,d,e) {}
+spp83<-function(a,b,c,d,e) {}
+spp84<-function(a,b,c,d,e) {}
+spp85<-function(a,b,c,d,e) {}
+spp86<-function(a,b,c,d,e) {}
+spp87<-function(a,b,c,d,e) {}
+spp88<-function(a,b,c,d,e) {}
+spp89<-function(a,b,c,d,e) {}
+spp90<-function(a,b,c,d,e) {}
+spp91<-function(a,b,c,d,e) {}
+spp92<-function(a,b,c,d,e) {}
+spp93<-function(a,b,c,d,e) {}
+spp94<-function(a,b,c,d,e) {}
+spp95<-function(a,b,c,d,e) {}
+spp96<-function(a,b,c,d,e) {}
+spp97<-function(a,b,c,d,e) {}
+spp98<-function(a,b,c,d,e) {}
+spp99<-function(a,b,c,d,e) {}
+spp100<-function(a,b,c,d,e) {}
